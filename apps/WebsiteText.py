87e7ed078d2f9a36ac82cc81228a0fed36127fa8
connbrack <80982854+connbrack@users.noreply.github.com>
2022-02-04 17:35:30 -0500

    working-no-auto-feature

--- apps/WebsiteText.py
+++
@@ -1,7 +0,0 @@
-
-def EnergyDashText():
-    textDict = {
-        "headingtext1": 'Welcome to the schedule energy estimator. Here you can assess the impact of proposed changes to the building schedule. The impact of the schedule is estimated based on the steam and chilled water data. To see this data, understand how the model was constructed and different test schedules click here.',
-        "headingtext2": 'To use this tool, first, verify that the estimated energy uses matches the building’s utility data; this will verify that the model has been constructed successfully. Since weather impacts building performance, you can normalize all years to a ‘typical meteorological year.’ Then you can adjust the schedule below to estimate the impact of schedule changes in 2020.'
-    }
-    return textDict
